443a88a81e658fe3d98980880cc2d03d57009e00
hyunjeongPark <GuoHJP@gmail.com>
2021-06-19 20:38:56 +0900

    HashRouter로 변경

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React, {memo, useEffect, useState} from 'react';
 import styles from './app.module.css';
 import './app.module.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/login/login';
 import Questions from './components/questions/questions';
 import Result from './components/result/result';
